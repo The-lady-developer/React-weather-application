9579d1c6dbdb9e4770947820d11c0a3aa475b403
The-lady-developer <manvisinghwal123@gmail.com>
2018-08-27 17:19:56 +0530

    weather-app

--- src/App.js
+++ src/App.js
@@ -21,20 +21,29 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
    
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
   const data = await api_call.json();
+  if (city && country){
   console.log(data);
-  //updating the value 
   this.setState({
-    temperature:data.main.temp,
-    city: data.name,
-    country: data.sys.country,
-    humidity:data.main.humidity,
-    description: data.weather[0].description,
-    error: ""
-
+ temperature: data.main.temp,
+ city: data.name,
+ country: data.sys.country,
+ humidity: data.main.humidity,
+ description: data.weather[0].description,
+ error: ""
   });
-  }
+}else {
+  this.setState({
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: "Please enter the value."
+     });
+}
+}
   render(){
     return(
       <div>
